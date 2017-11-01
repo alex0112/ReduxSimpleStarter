a1534831a31c1cfad5a2fe3dc1ccd4ca91dc9424
alex0112 <alarsen0112@gmail.com>
2017-11-01 05:41:02 -0600

    Defined searchbar component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCawtvv4TDwVdZx10MmnAIKFS9rQYsStyI';
 
 
-const App = () => {
-    return <div>Hi!</div>;
+
+class App extends Component {
+
+    constructor(props) {
+	super(props);
+
+	this.state = {videos: []};
+	YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+	    this.setState({ videos });
+	});
+	
+    }
+
+    render() {
+	return (
+		<div>
+		<SearchBar />
+		</div>
+	);
+    }
 }
 
 
